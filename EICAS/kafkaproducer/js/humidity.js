c5115d68c2d929817b6fd92b21d1d341bf419a78
mcandelieri <matias.candelieri@personal.com.ar>
2017-11-16 16:39:14 -0300

    Agrego servicios para humidity y elevation

--- EICAS/kafkaproducer/js/humidity.js
+++ EICAS/kafkaproducer/js/humidity.js
@@ -1,12 +1,14 @@
-var fec = require('./date.js'),
+exports.msghumidity = function () {
+  var fec = require('./date.js'),
 	kaf = require('./kafka.js'),
 	d = new Date,
 	sensor = Array("UTN0101","UTN0102","UTN0103"),
 	fecha = fec.formatDate(d),
 	topicName = 'humidity_topic';
 	
-for (var ms=[],i=0;i<3;++i)
+  for (var ms=[],i=0;i<3;++i)
 	ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"%", "fechaUltMod":"' + fecha +'","tipoProducto":"Humedad"}';
 
-kaf.insert(ms,topicName);
+  kaf.insert(ms,topicName);
 
+}
