f1a3922b26a2648ac54938a23388e01feffd0d1b
mcandelieri <matias.candelieri@personal.com.ar>
2017-11-16 17:00:43 -0300

    Cambio en index.js

--- EICAS/kafkaproducer/index.js
+++ EICAS/kafkaproducer/index.js
@@ -1,6 +1,6 @@
 var msgrec = require('./js/recarga.js');
-var msghum = require('./js/humidity.js);
-var msgele = require('./js/elevation.js);
+var msghum = require('./js/humidity.js');
+var msgele = require('./js/elevation.js');
 setInterval(msgrec.msgrecarga, 5000);
 setInterval(msghum.msghumidity,5000);
 setInterval(msgele.msgelevation,5000);
