587498dae4c9a9013147bd138d0ce696b288f437
Leonel Radaelli <radaellileonel@gmail.com>
2019-01-31 16:46:49 -0300

    AEI-6301 agrego log e elimino sleep

--- EICAS/kafkaproducer/js/infraAgent2.js
+++ EICAS/kafkaproducer/js/infraAgent2.js
@@ -1,21 +1,22 @@
 var monitor = require('os-monitor'),
-	disk = require('diskusage'),
-	hostname = require('os-hostname'),
-	sleep = require('sleep'),
-	array = [],
-	ms,
-	fec = require('./date.js'),
-	kaf = require('./kafka.js'),
-	topicName = 'infrastructure_topic';
+    disk = require('diskusage'),
+    hostname = require('os-hostname'),
+    sleep = require('sleep'),
+    array = [],
+    ms,
+    fec = require('./date.js'),
+    kaf = require('./kafka.js'),
+    topicName = 'infrastructure_topic';
 
 hostname(function (err, hname) {
-	array[0]=hname;
+    array[0] = hname;
 });
 
 
 exports.init = function () {
     setInterval(infraAgent2, 5000);
 }
+
 function infraAgent2() {
     monitor.start();
     monitor.on('monitor', function (event) {
@@ -30,10 +31,9 @@ function infraAgent2() {
             array[3] = ((info.total - info.available) / info.total) * 100;
         });
 
-        sleep.sleep(1);
 
         ms = '{"id" : 2023, "fechaUltMod":"' + fecha + '","tipoProducto" : "Monitoreo infraestructura", "tipoTerminal" : "' + array[0] + '", "unidad": "%", "idSensor" : 32,"valorCPU" : "' + Math.round(array[1]) + '", "valorMEM" : "' + Math.round(array[2]) + '", "valorFS" : "' + Math.round(array[3]) + '"}';
-
+        console.log(ms)
         kaf.insert(ms, topicName);
     });
 
