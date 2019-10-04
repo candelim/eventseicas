0af69157f84d31f70785304a04acb58e93601c2c
Matias <matias.candelieri@personal.com.ar>
2019-10-04 15:42:54 -0300

    Update temperature.js
    Cambio en el nombre del topic para el uso de la convención "-" en lugar de "_"

--- EICAS/kafkaproducer/js/temperature.js
+++ EICAS/kafkaproducer/js/temperature.js
@@ -4,7 +4,7 @@ exports.msgtemperature = function () {
 	d = new Date,
 	sensor = Array("UTN0101","UTN0102","UTN0103"),
 	fecha = fec.formatDate(d),
-	topicName = 'temperature_topic';
+	topicName = 'temperature-topic';
 	
   for (var ms=[],i=0;i<3;++i)
 	ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"C", "fechaUltMod":"' + fecha +'","tipoProducto":"Temperatura"}';
