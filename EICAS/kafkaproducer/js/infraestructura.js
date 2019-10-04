8e693c65ef9ce34f13c71d7d6700aa018db46882
Matias <matias.candelieri@personal.com.ar>
2019-10-04 15:43:20 -0300

    Update infraestructura.js
    Cambio en el nombre del topic para el uso de la convención "-" en lugar de "_"

--- EICAS/kafkaproducer/js/infraestructura.js
+++ EICAS/kafkaproducer/js/infraestructura.js
@@ -3,7 +3,7 @@ exports.msginfraestructura = function () {
         kaf = require('./kafka.js'),
         d = new Date,
         fecha = fec.formatDate(d),
-        topicName = 'infrastructure_topic';
+        topicName = 'infrastructure-topic';
 
   for (var ms=[],i=0;i<3;++i)
         ms[i] = '{"id" : 2023, "fechaUltMod":"' + fecha +'","tipoProducto" : "Monitoreo infraestructura", "tipoTerminal" : "HOST", "unidad": "%", "idSensor" : 32,"valorCPU" : "' + Math.floor((Math.random() * 50)) + '", "valorMEM" : "' + Math.floor((Math.random() * 50)) + '", "valorFS" : "' + Math.floor((Math.random() * 50)) + '"}';
