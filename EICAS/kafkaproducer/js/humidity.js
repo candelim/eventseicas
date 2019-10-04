6dba8e99aac14d70fda8878df023dab68a1a3f9a
Matias <matias.candelieri@personal.com.ar>
2019-10-04 15:41:43 -0300

    Update humidity.js
    Cambio en el nombre del topic para el uso de la convención "-" en lugar de "_"

--- EICAS/kafkaproducer/js/humidity.js
+++ EICAS/kafkaproducer/js/humidity.js
@@ -4,7 +4,7 @@ exports.msghumidity = function () {
 	d = new Date,
 	sensor = Array("UTN0101","UTN0102","UTN0103"),
 	fecha = fec.formatDate(d),
-	topicName = 'humidity_topic';
+	topicName = 'humidity-topic';
 	
   for (var ms=[],i=0;i<3;++i)
 	ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"%", "fechaUltMod":"' + fecha +'","tipoProducto":"Humedad"}';
