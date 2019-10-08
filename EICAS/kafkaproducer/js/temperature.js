9111a7b8fe0505aefe9a3418d3d4be1df96c4cff
Matias Candelieri <matias.candelieri@personal.com.ar>
2019-10-08 18:12:41 -0300

    Agrego variables de entorno y dejo solo prueba para topic temperature-topic. Agrego funcion de Stop

--- EICAS/kafkaproducer/js/temperature.js
+++ EICAS/kafkaproducer/js/temperature.js
@@ -4,9 +4,10 @@ exports.msgtemperature = function () {
 	d = new Date,
 	sensor = Array("UTN0101","UTN0102","UTN0103"),
 	fecha = fec.formatDate(d),
-	topicName = 'temperature-topic';
+	topicName = 'temperature-topic',
+	countmsg = process.env.COUNTMSG;
 	
-  for (var ms=[],i=0;i<3;++i)
+  for (var ms=[],i=0;i<countmsg;++i)
 	ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"C", "fechaUltMod":"' + fecha +'","tipoProducto":"Temperatura"}';
 
   kaf.insert(ms,topicName);
