8168ed98c5839abb9d2100155be16e9082328d1e
Leonel Radaelli <radaellileonel@gmail.com>
2019-10-11 16:32:47 -0300

    AEI-7849 nueva version

--- EICAS/kafkaproducer/js/temperature.js
+++ EICAS/kafkaproducer/js/temperature.js
@@ -1,13 +1,14 @@
 exports.msgtemperature = function () {
-  var fec = require('./date.js'),
-	kaf = require('./kafka.js'),
-	d = new Date,
-	sensor = Array("UTN0101","UTN0102","UTN0103"),
-	fecha = fec.formatDate(d),
-	topicName = 'temperature-topic-spark';
-	
-  for (var ms=[],i=0;i<3;++i)
-	ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"C", "fechaUltMod":"' + fecha +'","tipoProducto":"Temperatura"}';
+	var fec = require('./date.js'),
+		kaf = require('./kafka.js'),
+		d = new Date,
+		sensor = Array("UTN0101","UTN0102","UTN0103"),
+		fecha = fec.formatDate(d),
+		topicName = 'temperature-topic-spark';
+		countmsg = process.env.COUNTMSG;
 
-  kaf.insert(ms,topicName);
+	for (var ms=[],i=0;i<countmsg;++i)
+		ms[i] = '{"id":2023,"idSensor":"' + sensor[Math.floor(Math.random()*sensor.length)] +'","tipoTerminal":"SENSOR","valor":"'+ Math.floor((Math.random() * 20)) + '", "unidad":"C", "fechaUltMod":"' + fecha +'","tipoProducto":"Temperatura"}';
+
+	kaf.insert(ms,topicName);
 }
